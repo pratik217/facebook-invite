3a8cb9deaf4767318d26549379e50fdc5cb0c03b
pratik <pratik@localhost.localdomain>
2015-01-24 15:40:57 +0530

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^$', 'home.views.index'),                   
+    url(r'^admin/', include(admin.site.urls)),
+)
